abfa423904a3008e09793a5683529317bdcd5253
Braga Lund <bragalund@gmail.com>
2021-11-26 22:18:13 +0100

    Added better test

--- backend/bragapi-tests/Controllers/WeatherForecastControllerTests.cs
+++ backend/bragapi-tests/Controllers/WeatherForecastControllerTests.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using bragapi;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Hosting;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace bragapi_tests.Controllers
@@ -19,6 +23,9 @@ namespace bragapi_tests.Controllers
             var response = await client.GetAsync("/WeatherForecast");
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var jsonString = await response.Content.ReadAsStringAsync();
+            IEnumerable<WeatherForecast> weatherForecasts = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(jsonString);
+            Assert.True(weatherForecasts.Any());
         }
         class BragaApiApplication : WebApplicationFactory<Program>
         {
